6fa0a21f2123b70ce981755cf2c83174909c6b35
surnan <forsuresh@yandex.com>
2018-12-04 15:59:47 -0500

    topImageContainerView = newly created Container View imageView = added to topImageContainerView as formatting trick to resize image on rotations descriptionTextView - uses NSAttributedString

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -12,15 +12,26 @@ class ViewController: UIViewController {
 
     let bearImageView: UIImageView = {
         let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     let descriptionTextView: UITextView = {
-       let textView = UITextView()
-        textView.text = "Welcome, Hero"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        let textView = UITextView()
+        //        textView.text = "Welcome, Hero"
+        //        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        
+        let attributedText: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth: -4.6
+        ]
+        
+        textView.attributedText = NSMutableAttributedString(string: "Welcome, HERO", attributes: attributedText)
+        
         textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
@@ -28,14 +39,25 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    let topImageContainerView: UIView = {
+        let image = UIView()
+        image.backgroundColor = UIColor.blue
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
     fileprivate func setupLayout() {
         NSLayoutConstraint.activate([
-            bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            bearImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            bearImageView.widthAnchor.constraint(equalToConstant: 200),
-            bearImageView.heightAnchor.constraint(equalToConstant: 200),
+            topImageContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+            topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
-            descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120),
+            bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor),
+            bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor),
+            bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.6),
+            
+            descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 60),
             descriptionTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -45,8 +67,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        [bearImageView, descriptionTextView].forEach{view.addSubview($0)}
+        
+        
+        topImageContainerView.addSubview(bearImageView)
+        
+        
+        
+        
+        
+        
+        [descriptionTextView, topImageContainerView].forEach{view.addSubview($0)}
         setupLayout()
     }
+    
+    func setupTextField(textField: UITextField) -> UITextField{
+        let tempTextField = textField
+        tempTextField.clearsOnBeginEditing = true
+        let memeTextAttributes:[NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth: -4.6
+        ]
+        tempTextField.defaultTextAttributes = memeTextAttributes
+        tempTextField.textAlignment = .center
+//        tempTextField.delegate = self
+        return tempTextField
+    }
 }
+
+
 
